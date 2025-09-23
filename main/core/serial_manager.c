0b3b33b1014ffd9c900392f3b34c6d74869536a7
jay candel <133104319+jaylikesbunda@users.noreply.github.com>
2025-09-23 16:45:07 +0800

    disable uart serial on somethingsomething

--- main/core/serial_manager.c
+++ main/core/serial_manager.c
@@ -34,6 +34,7 @@
 char serial_buffer[SERIAL_BUFFER_SIZE];
 static TaskHandle_t s_serial_task_handle = NULL;
 static bool s_serial_initialized = false;
+static bool s_uart_disabled = false; // disable main serial UART for certain templates
 
 // HTML capture state
 typedef enum {
@@ -88,8 +89,10 @@ void serial_task(void *pvParameter) {
     int length = 0;
 
 #ifndef CONFIG_USE_IO_EXPANDER
-    // Read data from the main UART
-    length = uart_read_bytes(UART_NUM, data, BUF_SIZE, 10 / portTICK_PERIOD_MS);
+    // Read data from the main UART (if not disabled)
+    if (!s_uart_disabled) {
+      length = uart_read_bytes(UART_NUM, data, BUF_SIZE, 10 / portTICK_PERIOD_MS);
+    }
 #endif
 
 #if JTAG_SUPPORTED
@@ -108,7 +111,7 @@ void serial_task(void *pvParameter) {
             index--;
             // Echo backspace: write backspace, space, backspace directly to UART
             const char backspace_seq[] = "\b \b";
-            uart_write_bytes(UART_NUM, backspace_seq, 3);
+            if (!s_uart_disabled) uart_write_bytes(UART_NUM, backspace_seq, 3);
 #if JTAG_SUPPORTED
             usb_serial_jtag_write_bytes((const uint8_t*)backspace_seq, 3, 0);
 #endif
@@ -119,7 +122,7 @@ void serial_task(void *pvParameter) {
         if (incoming_char == '\n' || incoming_char == '\r') {
           // Echo newline directly to UART
           const char newline[] = "\n";
-          uart_write_bytes(UART_NUM, newline, 1);
+          if (!s_uart_disabled) uart_write_bytes(UART_NUM, newline, 1);
 #if JTAG_SUPPORTED
           usb_serial_jtag_write_bytes((const uint8_t*)newline, 1, 0);
 #endif
@@ -134,7 +137,7 @@ void serial_task(void *pvParameter) {
         if ((unsigned char)incoming_char >= 32 && (unsigned char)incoming_char != 127) {
           if (index < SERIAL_BUFFER_SIZE - 1) {
             // Echo the character immediately via direct UART write
-            uart_write_bytes(UART_NUM, &incoming_char, 1);
+            if (!s_uart_disabled) uart_write_bytes(UART_NUM, &incoming_char, 1);
 #if JTAG_SUPPORTED
             usb_serial_jtag_write_bytes((const uint8_t*)&incoming_char, 1, 0);
 #endif
@@ -160,6 +163,11 @@ void serial_task(void *pvParameter) {
 
 // Initialize the SerialManager
 void serial_manager_init() {
+#ifdef CONFIG_BUILD_CONFIG_TEMPLATE
+  if (strcmp(CONFIG_BUILD_CONFIG_TEMPLATE, "somethingsomething") == 0) {
+    s_uart_disabled = true;
+  }
+#endif
 #ifndef CONFIG_USE_IO_EXPANDER
   // UART configuration for main UART
   const uart_config_t uart_config = {
@@ -170,8 +178,10 @@ void serial_manager_init() {
       .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
   };
 
-  uart_param_config(UART_NUM, &uart_config);
-  uart_driver_install(UART_NUM, BUF_SIZE * 2, 0, 0, NULL, 0);
+  if (!s_uart_disabled) {
+    uart_param_config(UART_NUM, &uart_config);
+    uart_driver_install(UART_NUM, BUF_SIZE * 2, 0, 0, NULL, 0);
+  }
 #endif
 
 #if JTAG_SUPPORTED
@@ -186,7 +196,11 @@ void serial_manager_init() {
 
   xTaskCreate(serial_task, "SerialTask", 8192, NULL, 2, &s_serial_task_handle);
   s_serial_initialized = true;
-  glog("Serial Started...\n");
+  if (!s_uart_disabled) {
+    glog("Serial Started...\n");
+  } else {
+    glog("Serial Disabled (template: somethingsomething)\n");
+  }
 }
 
 void serial_manager_deinit() {
