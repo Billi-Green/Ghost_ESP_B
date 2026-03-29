f196983bfe65310abf094742b174e69ce68ab233
deki <partners@ghostesp.net>
2026-03-29 09:23:04 +0800

    gcc format

--- components/lvgl_esp32_drivers/lvgl_tft/st7735s.h
+++ components/lvgl_esp32_drivers/lvgl_tft/st7735s.h
@@ -23,7 +23,9 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
+#ifndef DISP_BUF_SIZE
 #define DISP_BUF_SIZE (LV_HOR_RES_MAX * 40)
+#endif
 
 #define ST7735S_DC       CONFIG_LV_DISP_PIN_DC
 #define ST7735S_RST      CONFIG_LV_DISP_PIN_RST
